ea85abd644dfcc209cac4fa26751451bbf3c7087
Sergio Castillo <daw04.2022@gmail.com>
2025-03-26 22:09:55 +0100

    Feat: section about

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {ButtonDirective} from 'primeng/button';
 import {Image} from 'primeng/image';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
     ButtonDirective,
-    Image
+    Image,
+    NgOptimizedImage
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
