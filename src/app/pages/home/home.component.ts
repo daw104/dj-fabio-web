58841cb1ad9d049b3141bb4a8f2d2f794ff50d9c
Sergio Castillo <daw04.2022@gmail.com>
2025-03-27 12:23:59 +0100

    Fix: section social-media and add font-awesome

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,16 @@
 import {Component} from '@angular/core';
 import {ButtonDirective} from 'primeng/button';
 import {Image} from 'primeng/image';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
     ButtonDirective,
     Image,
-    NgOptimizedImage
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  email: string = 'fabiofasciano97@gmail.com';
 }
