cccf9d3b5b6808a12cdc5a51bfd55cd4559b982d
Sergio Castillo <daw04.2022@gmail.com>
2025-03-27 16:39:54 +0100

    Feat: anchors tags from home component to services component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {ButtonDirective} from 'primeng/button';
 import {Image} from 'primeng/image';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
     ButtonDirective,
     Image,
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {ButtonDirective} from "primeng/button";
+import {ActivatedRoute} from '@angular/router';
+import {ViewportScroller} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-services',
@@ -9,6 +12,24 @@ import {ButtonDirective} from "primeng/button";
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
-export class ServicesComponent {
+export class ServicesComponent implements AfterViewInit {
+
+
+  constructor(private route: ActivatedRoute,
+              private viewportScroller: ViewportScroller,
+              private router: Router) {
+  }
+
+
+  ngAfterViewInit() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        // Usar ViewportScroller para hacer scroll
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(fragment);
+        }, 10); // Pequeño retraso para asegurar que la vista esté lista
+      }
+    });
+  }
 
 }
